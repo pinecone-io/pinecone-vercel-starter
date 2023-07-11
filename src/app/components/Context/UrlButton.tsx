c47867e778e0ef69b1c2214b09106256ab3764f7
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-07-11 12:48:28 -0700

    Render buttons

---
+++ src/app/components/Context/UrlButton.tsx
@@ -0,0 +1,42 @@
+import { Button } from "./Button";
+import React, { FC } from "react";
+
+export interface IUrlEntry {
+  url: string;
+  title: string;
+  seeded: boolean;
+  loading: boolean;
+}
+
+interface IURLButtonProps {
+  entry: IUrlEntry;
+}
+
+const UrlButton: FC<IURLButtonProps> = ({ entry }) => (
+  <div key={`${entry.url}-${entry.seeded}`} className="flex-grow px-2">
+    <Button
+      className={`relative overflow-hidden w-full my-2 mx-2 ${
+        entry.loading ? "shimmer" : ""
+      }`}
+      style={{
+        backgroundColor: entry.seeded ? "green" : "bg-gray-800",
+        color: entry.seeded ? "white" : "text-gray-200",
+      }}
+    >
+      {entry.loading && (
+        <div
+          className="absolute inset-0"
+          style={{
+            zIndex: -1,
+            background:
+              "linear-gradient(90deg, transparent, rgba(255,255,255,0.5), transparent)",
+            animation: "shimmer 2s infinite",
+          }}
+        ></div>
+      )}
+      <div className="relative">{entry.title}</div>
+    </Button>
+  </div>
+);
+
+export default UrlButton;
--- src/app/components/Context/index.tsx
+++ src/app/components/Context/index.tsx
@@ -1,18 +1,22 @@
 import React from "react";
+import { urls } from "./urls";
+import UrlButton from "./UrlButton";
 interface ContextProps {
   className: string;
 }
 
 export const Context: React.FC<ContextProps> = ({ className }) => {
+  const [entries, setEntries] = React.useState(urls);
+
+  const buttons = entries.map((entry, key) => (
+    <UrlButton key={key} entry={entry} />
+  ));
   return (
     <div className="flex flex-col space-y-4 overflow-y-scroll items-center">
       <div className="flex flex-col items-center sticky top-0">
+        <div className="flex p-2">{buttons}</div>
         <div className="flex p-2">
-          <div className="flex-grow px-2">
-            <div className="flex flex-col items-center text-white text-xl">
-              Context
-            </div>
-          </div>
+          <div className="flex-grow px-2"></div>
         </div>
       </div>
     </div>
