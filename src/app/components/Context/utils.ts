4b705266ec1ccb05bbcce699e1a889f784fbe2ce
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-07-11 12:56:54 -0700

    Crawl document call

---
+++ src/app/components/Context/utils.ts
@@ -0,0 +1,39 @@
+import { IUrlEntry } from "./UrlButton";
+import { ICard } from "./Card";
+
+export async function crawlDocument(
+  url: string,
+  setEntries: React.Dispatch<React.SetStateAction<IUrlEntry[]>>,
+  setCards: React.Dispatch<React.SetStateAction<ICard[]>>,
+  splittingMethod: string,
+  chunkSize: number,
+  overlap: number
+): Promise<void> {
+  setEntries((seeded: IUrlEntry[]) =>
+    seeded.map((seed: IUrlEntry) =>
+      seed.url === url ? { ...seed, loading: true } : seed
+    )
+  );
+  const response = await fetch("/api/crawl", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      url,
+      options: {
+        splittingMethod,
+        chunkSize,
+        overlap,
+      },
+    }),
+  });
+
+  const { documents } = await response.json();
+
+  setCards(documents);
+
+  setEntries((prevEntries: IUrlEntry[]) =>
+    prevEntries.map((entry: IUrlEntry) =>
+      entry.url === url ? { ...entry, seeded: true, loading: false } : entry
+    )
+  );
+}
