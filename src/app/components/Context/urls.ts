915cd6e61600297e65d02e5efd0ce498d4ab151a
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-07-11 12:39:08 -0700

    Added URLs

---
+++ src/app/components/Context/urls.ts
@@ -0,0 +1,18 @@
+export const urls = [{
+  url: "https://e360.yale.edu/digest/indonesia-malaysia-deforestation",
+  title: "Indonesia Deforestation",
+  seeded: false,
+  loading: false,
+},
+{
+  url: "https://cleantechnica.com/2023/06/29/solar-82-of-power-capacity-growth-in-india-in-2022/",
+  title: "Solar Power in India",
+  seeded: false,
+  loading: false,
+},
+{
+  url: "https://www.cbssports.com/nba/news/matisse-thybulle-to-stay-with-blazers-who-match-mavericks-three-year-33-million-deal-per-report/",
+  title: "Matisee Thybulle",
+  seeded: false,
+  loading: false,
+}]
