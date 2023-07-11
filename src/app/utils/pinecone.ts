e44fc9172df6b9602f68f7c889f5a2178eb8bb4f
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-07-11 12:14:00 -0700

    init pinecone client

---
+++ src/app/utils/pinecone.ts
@@ -0,0 +1,14 @@
+import { PineconeClient, ScoredVector } from "@pinecone-database/pinecone";
+
+let pinecone: PineconeClient | null = null;
+
+export const getPineconeClient = async () => {
+  if (!pinecone) {
+    pinecone = new PineconeClient();
+    await pinecone.init({
+      environment: process.env.PINECONE_ENVIRONMENT!,
+      apiKey: process.env.PINECONE_API_KEY!,
+    });
+  }
+  return pinecone
+}
