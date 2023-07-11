23f6c1296923d90b10d83261aa5fb53958919126
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-07-11 12:54:07 -0700

    Added dropdown

--- src/app/components/Context/index.tsx
+++ src/app/components/Context/index.tsx
@@ -17,6 +17,83 @@ export const Context: React.FC<ContextProps> = ({ className }) => {
     },
   ]);
 
+  const [splittingMethod, setSplittingMethod] = useState("markdown");
+  const [chunkSize, setChunkSize] = useState(256);
+  const [overlap, setOverlap] = useState(1);
+
+  const DropdownLabel: React.FC<
+    React.PropsWithChildren<{ htmlFor: string }>
+  > = ({ htmlFor, children }) => (
+    <label htmlFor={htmlFor} className="text-white p-2 font-bold">
+      {children}
+    </label>
+  );
+
+  const DropdownInput: React.FC<{
+    value: number;
+    id: string;
+    min: number;
+    max: number;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  }> = ({ value, id, min, max, onChange }) => (
+    <input
+      className="p-2 bg-gray-700"
+      type="range"
+      id={id}
+      min={min}
+      max={max}
+      value={value}
+      onChange={onChange}
+    />
+  );
+
+  const Dropdown: React.FC = () => {
+    return (
+      <div className="text-left w-full ml-1 mr-1 flex flex-col bg-gray-600 p-3  subpixel-antialiased">
+        <DropdownLabel htmlFor="splittingMethod">
+          Splitting Method:
+        </DropdownLabel>
+        <select
+          id="splittingMethod"
+          value={splittingMethod}
+          className="p-2 bg-gray-700 rounded text-white"
+          onChange={(e) => setSplittingMethod(e.target.value)}
+        >
+          <option value="recursive">Recursive Text Splitting</option>
+          <option value="markdown">Markdown Splitting</option>
+        </select>
+        {splittingMethod === "recursive" && (
+          <div className="my-4 flex flex-col">
+            <div className="flex flex-col w-full">
+              <DropdownLabel htmlFor="chunkSize">
+                Chunk Size: {chunkSize}
+              </DropdownLabel>
+              <DropdownInput
+                value={chunkSize}
+                id="chunkSize"
+                min={1}
+                max={2048}
+                onChange={(e) => setChunkSize(parseInt(e.target.value))}
+              />
+            </div>
+            <div className="flex flex-col w-full">
+              <DropdownLabel htmlFor="overlap">
+                Overlap: {overlap}
+              </DropdownLabel>
+              <DropdownInput
+                value={overlap}
+                id="overlap"
+                min={1}
+                max={200}
+                onChange={(e) => setOverlap(parseInt(e.target.value))}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  };
+
   const buttons = entries.map((entry, key) => (
     <UrlButton key={key} entry={entry} />
   ));
@@ -27,6 +104,7 @@ export const Context: React.FC<ContextProps> = ({ className }) => {
         <div className="flex p-2">
           <div className="flex-grow px-2"></div>
         </div>
+        <Dropdown />
       </div>
       <div className="flex flex-wrap w-full">
         {cards &&
