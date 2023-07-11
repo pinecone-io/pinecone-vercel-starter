7370617aef582aaa87a8e5f09350788c22308467
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-07-11 13:50:18 -0700

    Clear index back end

---
+++ src/app/api/clearIndex/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getPineconeClient } from "@/app/utils/pinecone";
+
+export async function POST(req: Request) {
+  const pinecone = await getPineconeClient()
+  const index = pinecone.Index(process.env.PINECONE_INDEX!)
+  await index.delete1({
+    deleteAll: true
+  });
+  return NextResponse.json({
+    success: true
+  })
+}
