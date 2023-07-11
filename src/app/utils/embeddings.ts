397a5dd27dade735f0737ea4ae4d1adf7f59beb0
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-07-11 12:15:30 -0700

    Get Embeddings

---
+++ src/app/utils/embeddings.ts
@@ -0,0 +1,23 @@
+
+import { OpenAIApi, Configuration } from "openai-edge";
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+})
+const openai = new OpenAIApi(config)
+
+export async function getEmbeddings(input: string) {
+  try {
+    const response = await openai.createEmbedding({
+      model: "text-embedding-ada-002",
+      input,
+    })
+
+    const result = await response.json();
+    return result.data[0].embedding as number[]
+
+  } catch (e) {
+    console.log("Error calling OpenAI embedding API: ", e);
+    throw new Error(`Error calling OpenAI embedding API: ${e}`);
+  }
+}
