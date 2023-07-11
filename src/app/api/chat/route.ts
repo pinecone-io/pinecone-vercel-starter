f42450d0da3a125cbe2b84eb1827213a3b759e30
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-07-11 12:24:38 -0700

    Update the prompt

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -32,6 +32,13 @@ export async function POST(req: Request) {
       AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
       AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
       AI assistant is a big fan of Pinecone and Vercel.
+      START CONTEXT BLOCK
+      ${context}
+      END OF CONTEXT BLOCK
+      AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
+      If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
+      AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
+      AI assistant will not invent anything that is not drawn directly from the context.
       `,
       },
     ]
