d32574aff472b788cb5436608192f4c82783c1aa
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-07-11 13:02:57 -0700

    Context endpoint

---
+++ src/app/api/context/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import { getContext } from "@/app/utils/context";
+import { ScoredVector } from "@pinecone-database/pinecone";
+
+export async function POST(req: Request) {
+  try {
+    const { messages } = await req.json()
+    const lastMessage = messages.length > 1 ? messages[messages.length - 1] : messages[0]
+    const context = await getContext(lastMessage.content, '', 10000, 0.7, false) as ScoredVector[]
+    return NextResponse.json({ context })
+  } catch (e) {
+    console.log(e)
+    return NextResponse.error()
+  }
+}
