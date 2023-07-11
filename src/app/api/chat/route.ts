8b6b3f59e7301d3f5ae325d5d4c8db899b034936
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-07-11 10:59:44 -0700

    Chat backend

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,45 @@
+import { Configuration, OpenAIApi } from 'openai-edge'
+import { Message, OpenAIStream, StreamingTextResponse } from 'ai'
+
+// Create an OpenAI API client (that's edge friendly!)
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+})
+const openai = new OpenAIApi(config)
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge'
+
+export async function POST(req: Request) {
+  try {
+
+    const { messages } = await req.json()
+
+
+    const prompt = [
+      {
+        role: 'system',
+        content: `AI assistant is a brand new, powerful, human-like artificial intelligence.
+      The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
+      AI is a well-behaved and well-mannered individual.
+      AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
+      AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
+      AI assistant is a big fan of Pinecone and Vercel.
+      `,
+      },
+    ]
+
+    // Ask OpenAI for a streaming chat completion given the prompt
+    const response = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      stream: true,
+      messages: [...prompt, ...messages.filter((message: Message) => message.role === 'user')]
+    })
+    // Convert the response into a friendly text-stream
+    const stream = OpenAIStream(response)
+    // Respond with the stream
+    return new StreamingTextResponse(stream)
+  } catch (e) {
+    throw (e)
+  }
+}
