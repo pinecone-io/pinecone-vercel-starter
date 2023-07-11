77eea4b9187d2505d6223c43b9bfb22117bf38b3
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-07-11 12:14:51 -0700

    Get matches from embeddings

--- src/app/utils/pinecone.ts
+++ src/app/utils/pinecone.ts
@@ -12,3 +12,40 @@ export const getPineconeClient = async () => {
   }
   return pinecone
 }
+
+// The function `getMatchesFromEmbeddings` is used to retrieve matches for the given embeddings
+const getMatchesFromEmbeddings = async (embeddings: number[], topK: number, namespace: string): Promise<ScoredVector[]> => {
+  // Obtain a client for Pinecone
+  const pinecone = await getPineconeClient();
+
+  // Retrieve the list of indexes
+  const indexes = await pinecone.listIndexes()
+
+  // Check if the desired index is present, else throw an error
+  if (!indexes.includes(process.env.PINECONE_INDEX!)) {
+    throw (new Error(`Index ${process.env.PINECONE_INDEX} does not exist`))
+  }
+
+  // Get the Pinecone index
+  const index = pinecone!.Index(process.env.PINECONE_INDEX!);
+
+  // Define the query request
+  const queryRequest = {
+    vector: embeddings,
+    topK,
+    includeMetadata: true,
+    namespace
+  }
+
+  try {
+    // Query the index with the defined request
+    const queryResult = await index.query({ queryRequest })
+    return queryResult.matches || []
+  } catch (e) {
+    // Log the error and throw it
+    console.log("Error querying embeddings: ", e)
+    throw (new Error(`Error querying embeddings: ${e}`,))
+  }
+}
+
+export { getMatchesFromEmbeddings }
