15be48cb717d08914c13f824429335b80410a71a
harounans <hansari97@gmail.com>
2023-09-24 01:32:10 -0400

    revert prompt

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -36,7 +36,9 @@ export async function POST(req: Request) {
       ${context}
       END OF CONTEXT BLOCK
       AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
+      If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
       AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
+      AI assistant will not invent anything that is not drawn directly from the context.
       `,
       },
     ]
