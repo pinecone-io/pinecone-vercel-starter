0e6958f2e26edc6580283a0ab153370a988b6754
Zachary Proser <zackproser@gmail.com>
2023-09-29 20:21:13 +0000

    Define Metadata type and pass it to Pinecone index

--- src/app/utils/pinecone.ts
+++ src/app/utils/pinecone.ts
@@ -1,27 +1,30 @@
 import { Pinecone, type ScoredPineconeRecord } from "@pinecone-database/pinecone";
 
+export type Metadata = {
+  url: string,
+  text: string,
+  chunk: string,
+  hash: string
+}
+
 // The function `getMatchesFromEmbeddings` is used to retrieve matches for the given embeddings
 const getMatchesFromEmbeddings = async (embeddings: number[], topK: number, namespace: string): Promise<ScoredPineconeRecord<Metadata>[]> => {
   // Obtain a client for Pinecone
   const pinecone = new Pinecone();
 
-  // Retrieve the list of indexes
-  const indexes = await pinecone.listIndexes()
-
-  let exists = false
-  for (const index of indexes) {
-    if (index.name === process.env.PINECONE_INDEX!) {
-      exists = true
-    }
+  const indexName: string = process.env.PINECONE_INDEX || '';
+  if (indexName === '') {
+    throw new Error('PINECONE_INDEX environment variable not set')
   }
 
-  // Check if the desired index is present, else throw an error
-  if (!exists) {
-    throw (new Error(`Index ${process.env.PINECONE_INDEX} does not exist`))
+  // Retrieve the list of indexes to check if expected index exists
+  const indexes = await pinecone.listIndexes()
+  if (indexes.filter(i => i.name === indexName).length !== 1) {
+    throw new Error(`Index ${indexName} does not exist`)
   }
 
   // Get the Pinecone index
-  const index = pinecone!.Index(process.env.PINECONE_INDEX!);
+  const index = pinecone!.Index<Metadata>(indexName);
 
   // Get the namespace
   const pineconeNamespace = index.namespace(namespace ?? '')
